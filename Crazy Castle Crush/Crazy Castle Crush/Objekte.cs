7db1321278c5279f035a638afd22b823ab785ef0
konrad <konrad@tum.de>
2013-11-27 14:11:11 +0100

    Mit Object.cs

---
+++ Crazy Castle Crush/Crazy Castle Crush/Objekte.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NOVA.Scenery;
+using NOVA.UI;
+using NOVA.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Crazy_Castle_Crush
+{
+    class Objekte
+    {
+        public Objekte(Scene scene)
+        {
+            this.scene = scene;
+        }
+
+        public void Geldanzeige(Spieler spieler)
+        {
+            
+            string aktuellerText = "$" + spieler.getMoney();
+            UI2DRenderer.WriteText(new Vector2(scene.Camera.Position.X + 2, scene.Camera.Position.Y),           //Position
+            aktuellerText,                                                                          //Anzuzeigender Text
+            Color.Red,                                                                              //Textfarbe
+            null,                                                                                   //Interne Schriftart verwenden
+            Vector2.One,                                                                            //Textskallierung
+            UI2DRenderer.HorizontalAlignment.Left,                                                  //Horizontal zentriert
+            UI2DRenderer.VerticalAlignment.Top);                                                    //am unteren Bildschirmrand ausrichten
+
+        }
+
+
+
+
+
+
+
+        private Scene scene;
+    }
+}
